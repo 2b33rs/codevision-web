3b68290d506908eed6df386c74e3d823d4c6ac86
2b33rs <tobiasbiehl@icloud.com>
2025-04-08 21:23:51 +0200

    add backend to env

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,21 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export let GLOBAL_API_TOKEN: null | string = null;
-
-export function setGlobalApiToken(token: string) {
-  GLOBAL_API_TOKEN = token;
-}
-
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/",
-    // prepareHeaders: (headers) => {
-    //   if (GLOBAL_API_TOKEN) {
-    //     headers.set("Authorization", "Bearer " + GLOBAL_API_TOKEN);
-    //   }
-    //   return headers;
-    // },
+    baseUrl: import.meta.env.VITE_API_URL || "http://localhost:8080/", // localhost can only be used for local dev
   }),
   tagTypes: ["User", "Order"],
   refetchOnReconnect: true,
