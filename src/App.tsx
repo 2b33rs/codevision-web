47ec11d4d08645d4740b8056f7476f7603324330
2b33rs <tobiasbiehl@icloud.com>
2025-04-30 11:42:51 +0200

    Add icon and meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,21 @@ import { RouterProvider } from "react-router";
 import { store } from "@/api/store.ts";
 import { Provider } from "react-redux";
 import { ErrorBoundary } from "@/navigation/ErrorBoundary.tsx";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Verkauf & Versand | YourShirt GmbH";
+    const metaDesc = document.querySelector('meta[name="description"]');
+    if (metaDesc) {
+      metaDesc.setAttribute("content", "Übersicht Verkauf und Versand");
+    } else {
+      const meta = document.createElement("meta");
+      meta.name = "description";
+      meta.content = "Übersicht Verkauf und Versand";
+      document.head.appendChild(meta);
+    }
+  });
   return (
     <ErrorBoundary>
       <Provider store={store}>
