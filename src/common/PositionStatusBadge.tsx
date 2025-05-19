58cc66a3773a1e3211335ddf9343266f1a42caf4
2b33rs <tobiasbiehl@icloud.com>
2025-05-19 16:22:40 +0200

    FIX status

--- src/common/PositionStatusBadge.tsx
+++ src/common/PositionStatusBadge.tsx
@@ -1,10 +1,18 @@
 import { Badge } from "@/components/ui/badge";
-import { Loader2, Truck, CheckCircle, Ban, SearchCheck } from "lucide-react";
+import {
+  IconAlertTriangle,
+  IconBoxSeam,
+  IconCheckupList,
+  IconCircleCheck,
+  IconLoader2,
+  IconTruckLoading,
+} from "@tabler/icons-react";
 import React from "react";
 
 export type PositionStatus =
   | "IN_PROGRESS"
   | "READY_FOR_SHIPMENT"
+  | "READY_FOR_INSPECTION"
   | "INSPECTED"
   | "COMPLETED"
   | "CANCELLED"
@@ -19,29 +27,34 @@ const config: Record<
   }
 > = {
   IN_PROGRESS: {
-    label: "In Bearbeitung",
+    label: "In Produktion",
     color: "bg-muted text-yellow-800",
-    icon: <Loader2 className="mr-1 h-4 w-4 animate-spin" />,
+    icon: <IconLoader2 className="mr-1 h-4 w-4 animate-spin" />,
+  },
+  READY_FOR_INSPECTION: {
+    label: "Zur Prüfung ausgelagert",
+    color: "bg-purple-100 text-purple-800",
+    icon: <IconTruckLoading className="mr-1 h-4 w-4" />,
   },
   READY_FOR_SHIPMENT: {
-    label: "Bereit für Versand",
+    label: "Fertig produziert",
     color: "bg-blue-100 text-blue-800",
-    icon: <Truck className="mr-1 h-4 w-4" />,
+    icon: <IconBoxSeam className="mr-1 h-4 w-4" />,
   },
   INSPECTED: {
-    label: "Geprüft",
+    label: "Geprüft & freigegeben",
     color: "bg-purple-100 text-purple-800",
-    icon: <SearchCheck className="mr-1 h-4 w-4" />,
+    icon: <IconCheckupList className="mr-1 h-4 w-4" />,
   },
   COMPLETED: {
-    label: "Versendet",
+    label: "Versendet an Kunden",
     color: "bg-green-100 text-green-800",
-    icon: <CheckCircle className="mr-1 h-4 w-4" />,
+    icon: <IconCircleCheck className="mr-1 h-4 w-4" />,
   },
   CANCELLED: {
-    label: "Reklamiert",
+    label: "Reklamiert / abgebrochen",
     color: "bg-red-100 text-red-800",
-    icon: <Ban className="mr-1 h-4 w-4" />,
+    icon: <IconAlertTriangle className="mr-1 h-4 w-4" />,
   },
 };
 
@@ -50,7 +63,6 @@ function isKnownStatus(
 ): status is Exclude<PositionStatus, string> {
   return Object.prototype.hasOwnProperty.call(config, status);
 }
-
 
 export function PositionStatusBadge({ status }: { status: PositionStatus }) {
   if (!isKnownStatus(status)) status = "IN_PROGRESS";
--- src/feature/produced-order/VisualCheckTable.tsx
+++ src/feature/produced-order/VisualCheckTable.tsx
@@ -3,56 +3,52 @@ import { orderApi } from "@/api/endpoints/orderApi.ts";
 import SelectablePositionsTable from "@/feature/produced-order/SelectPositionTable.tsx";
 import { toast } from "sonner";
 import buildComposeId from "@/common/Utils.ts";
-import {Position} from "@/models/order.ts";
+import { Position } from "@/models/order.ts";
 
 const VisualCheckTable = () => {
   const { data: producedOrders = [], refetch } =
-      orderApi.useGetOrdersWithPositionStatusQuery("COMPLETED"); //READ_FOR_INSPECTION sobald es implementiert ist
+    orderApi.useGetOrdersWithPositionStatusQuery("READY_FOR_INSPECTION");
 
   const [refreshCounter, setRefreshCounter] = useState(0);
 
   const ordersWithCount = producedOrders.map((order) => {
     const readyCount = order.positions.filter(
-        (pos) => pos.Status === "COMPLETED"
+      (pos) => pos.Status === "READY_FOR_INSPECTION",
     ).length;
     return { ...order, readyCount };
   });
 
   const sortedOrders = ordersWithCount.sort((a, b) =>
-      a.orderNumber.localeCompare(b.orderNumber)
+    a.orderNumber.localeCompare(b.orderNumber),
   );
 
-
   const handleStatusChange = async (
-      selected: Position[],
-      orderNumber: string,
-      status: "INSPECTED" | "CANCELLED"
+    selected: Position[],
+    orderNumber: string,
+    status: "INSPECTED" | "CANCELLED",
   ) => {
     try {
       const responses = await Promise.all(
-          selected.map((position: Position) => {
-            const compositeId = buildComposeId(
-                orderNumber,
-                position.pos_number
-            );
-            return fetch(
-                `https://codevision-backend-production.up.railway.app/position/${compositeId}`,
-                {
-                  method: "PATCH",
-                  headers: {
-                    "Content-Type": "application/json",
-                  },
-                  body: JSON.stringify({ status }),
-                }
-            );
-          })
+        selected.map((position: Position) => {
+          const compositeId = buildComposeId(orderNumber, position.pos_number);
+          return fetch(
+            `https://codevision-backend-production.up.railway.app/position/${compositeId}`,
+            {
+              method: "PATCH",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ status }),
+            },
+          );
+        }),
       );
 
       const allSuccessful = responses.every((res) => res.ok);
 
       if (allSuccessful) {
         toast.success(
-            `${selected.length} Position(en) aus Order #${orderNumber} erfolgreich auf "${status}" gesetzt.`
+          `${selected.length} Position(en) aus Order #${orderNumber} erfolgreich auf "${status}" gesetzt.`,
         );
         await refetch();
         setRefreshCounter((prev) => prev + 1); // 🔁 trigger Table reset
@@ -60,49 +56,49 @@ const VisualCheckTable = () => {
         toast.error("Einige Positionen konnten nicht aktualisiert werden.");
       }
     } catch (error) {
-      toast.error(
-          "Fehler beim PATCH-Request: " + (error as Error).message
-      );
+      toast.error("Fehler beim PATCH-Request: " + (error as Error).message);
     }
   };
 
   return (
-      <div className="space-y-6">
-        {sortedOrders.map((order) => (
-            <div key={order.id}>
-              <SelectablePositionsTable
-                  key={`${order.id}-${refreshCounter}`} // 🔁 forces reset
-                  positions={order.positions}
-                  orderNumber={order.orderNumber}
-                  selectableStatus={"COMPLETED"}
-                  actions={[
-                    {
-                      label: "Visueller Check durchgeführt",
-                      content: (selected, orderNumber) => (
-                          <div className="px-4 py-2 text-sm">
-                            {selected.length} Position(en) aus Order #{orderNumber} geprüft?
-                          </div>
-                      ),
-                      onConfirm: (selected, orderNumber) =>
-                          handleStatusChange(selected, orderNumber, "INSPECTED"),
-                      renderDropdown: true,
-                    },
-                    {
-                      label: "Reklamieren",
-                      content: (selected, orderNumber) => (
-                          <div className="px-4 py-2 text-sm">
-                            {selected.length} Position(en) aus Order #{orderNumber} reklamieren?
-                          </div>
-                      ),
-                      onConfirm: (selected, orderNumber) =>
-                          handleStatusChange(selected, orderNumber, "CANCELLED"),
-                      renderDropdown: true,
-                    },
-                  ]}
-              />
-            </div>
-        ))}
-      </div>
+    <div className="space-y-6">
+      {sortedOrders.map((order) => (
+        <div key={order.id}>
+          <SelectablePositionsTable
+            key={`${order.id}-${refreshCounter}`} // 🔁 forces reset
+            positions={order.positions}
+            orderNumber={order.orderNumber}
+            selectableStatus={"COMPLETED"}
+            actions={[
+              {
+                label: "Visueller Check durchgeführt",
+                content: (selected, orderNumber) => (
+                  <div className="px-4 py-2 text-sm">
+                    {selected.length} Position(en) aus Order #{orderNumber}{" "}
+                    geprüft?
+                  </div>
+                ),
+                onConfirm: (selected, orderNumber) =>
+                  handleStatusChange(selected, orderNumber, "INSPECTED"),
+                renderDropdown: true,
+              },
+              {
+                label: "Reklamieren",
+                content: (selected, orderNumber) => (
+                  <div className="px-4 py-2 text-sm">
+                    {selected.length} Position(en) aus Order #{orderNumber}{" "}
+                    reklamieren?
+                  </div>
+                ),
+                onConfirm: (selected, orderNumber) =>
+                  handleStatusChange(selected, orderNumber, "CANCELLED"),
+                renderDropdown: true,
+              },
+            ]}
+          />
+        </div>
+      ))}
+    </div>
   );
 };
 
