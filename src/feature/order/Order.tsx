b4bbe3f67714f863d09d6f50dc95750fac59a36c
Tostorium <acakir@stud.hs-offenburg.de>
2025-06-04 02:22:41 +0200

    Added "Reklamationen" tab in "Bestellungen". Invoice PDF looks better now.

--- src/feature/order/Order.tsx
+++ src/feature/order/Order.tsx
@@ -1,21 +1,38 @@
-import OrderTable from "@/feature/order/OrderTable.tsx";
-import { BaseContentLayout } from "@/common/BaseContentLayout.tsx";
 import React from "react";
 import { Dialog, DialogContent } from "@/components/ui/dialog.tsx";
+import OrderTable from "@/feature/order/OrderTable.tsx";
 import CreateOrderForm from "@/feature/order/CreateOrderForm.tsx";
+import { BaseContentLayout } from "@/common/BaseContentLayout.tsx";
+import BaseTabsLayout from "@/common/BaseTabsLayout.tsx";
+import InvoicesTable from "@/feature/order/InvoicesTable.tsx"
+
 
 const Order = () => {
-  const [showModal, setShowModal] = React.useState(false);
-  return (
-    <Dialog open={showModal} onOpenChange={setShowModal}>
-      <BaseContentLayout>
-        <OrderTable setShowModal={setShowModal} />
-      </BaseContentLayout>
-      <DialogContent className="w-[95vw] !max-w-[none] sm:w-[90vw] md:w-[80vw] lg:w-[1200px]">
-        <CreateOrderForm setShowModal={setShowModal} />
-      </DialogContent>
-    </Dialog>
-  );
+    const [showModal, setShowModal] = React.useState(false);
+
+    return (
+        <Dialog open={showModal} onOpenChange={setShowModal}>
+            <BaseContentLayout>
+
+                <BaseTabsLayout
+                    tabs={[
+                        {
+                            title: "Bestellungen",
+                            content: <OrderTable setShowModal={setShowModal} />,
+                        },
+                        {
+                            title: "Rechnungen",
+                            content: <InvoicesTable  />,
+                        },
+                    ]}
+                />
+            </BaseContentLayout>
+
+            <DialogContent className="w-[95vw] !max-w-[none] sm:w-[90vw] md:w-[80vw] lg:w-[1200px]">
+                <CreateOrderForm setShowModal={setShowModal} />
+            </DialogContent>
+        </Dialog>
+    );
 };
 
 export default Order;
