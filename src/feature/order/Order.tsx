5e96168bef0b7b81e8560d42e8c21376b3647836
Arda Cakir <131270604+Tostorium@users.noreply.github.com>
2025-06-06 16:31:28 +0200

    Update Order.tsx

--- src/feature/order/Order.tsx
+++ src/feature/order/Order.tsx
@@ -7,9 +7,11 @@ import BaseTabsLayout from "@/common/BaseTabsLayout.tsx";
 import InvoicesTable from "@/feature/order/InvoicesTable.tsx";
 
 const Order = () => {
-  const [showModal, setShowModal] = React.useState(false);
+    const [showModal, setShowModal] = React.useState(false);
 
-
+    return (
+        <Dialog open={showModal} onOpenChange={setShowModal}>
+            <BaseContentLayout>
                 <BaseTabsLayout
                     tabs={[
                         {
@@ -18,34 +20,17 @@ const Order = () => {
                         },
                         {
                             title: "Dokumente",
-                            content: <InvoicesTable  />,
+                            content: <InvoicesTable />,
                         },
                     ]}
                 />
             </BaseContentLayout>
 
-  return (
-    <Dialog open={showModal} onOpenChange={setShowModal}>
-      <BaseContentLayout>
-        <BaseTabsLayout
-          tabs={[
-            {
-              title: "Bestellungen",
-              content: <OrderTable setShowModal={setShowModal} />,
-            },
-            {
-              title: "Rechnungen",
-              content: <InvoicesTable />,
-            },
-          ]}
-        />
-      </BaseContentLayout>
-
-      <DialogContent className="w-[95vw] !max-w-[none] sm:w-[90vw] md:w-[80vw] lg:w-[1200px]">
-        <CreateOrderForm setShowModal={setShowModal} />
-      </DialogContent>
-    </Dialog>
-  );
+            <DialogContent className="w-[95vw] !max-w-[none] sm:w-[90vw] md:w-[80vw] lg:w-[1200px]">
+                <CreateOrderForm setShowModal={setShowModal} />
+            </DialogContent>
+        </Dialog>
+    );
 };
 
 export default Order;
